f38df637d5ac031c591cde554607d4f0bf2a6575
dlsso <supertonesfan@hotmail.com>
2014-07-25 12:22:14 -0600

    Grabbed form data and pushed to an array

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var submissions = []
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -14,6 +16,14 @@ app.get('/', function(req, res) {
 app.get('/submit.html', function(req, res) {
 	res.render('submit');
 });
+
+app.post('/submission', function(req, res){
+	var submission = req.body;
+	submissions.push(submission)
+	console.log(submissions)
+	res.redirect('/')
+})
+
 
 var server = app.listen(3000, function() {
 	console.log('Express server listening on port ' + server.address().port);
